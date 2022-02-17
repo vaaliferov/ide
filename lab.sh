6604a1c49716607172079c7e059ba5e3c1d23192
vaaliferov <vaaliferov@gmail.com>
2022-02-17 19:41:02 +0300

    update

---
+++ lab.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+USER=vaaliferov
+ENV_PATH=/home/$USER/dev
+WORK_DIR=/home/$USER/github
+
+source $ENV_PATH/bin/activate
+python3 -m jupyterlab --notebook-dir=$WORK_DIR
+deactivate
